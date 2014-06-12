49635476f24c3d3d79cc16d225d882246b21686f
zhbanchikk <zhbanchikk@github.com>
2014-06-11 23:12:19 -0400

    kapx hw

---
+++ dev/python/selenium/test.py
@@ -0,0 +1,69 @@
+###################################################################
+### Using PyUnit (Python standart library, module unittest)     ###
+### Created on 05/22/2014                                       ###
+### Author: V. Kovrigin                                         ###
+###################################################################
+
+# you might need to install 'selenium' package by executing e.g.
+# pip install selenium
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class KapXLogin(unittest.TestCase):
+
+    def setUp(self):
+        # initialization
+        self.driver = webdriver.Firefox()
+        self.base_url = "https://kapx.kaplan.com"
+
+    def test_kapx_login_and_logout(self):
+        driver = self.driver
+        # open home page
+        driver.get(self.base_url + "/")
+        # assert for the text "KAPx" in the title
+        self.assertIn("KAPx", driver.title)
+
+        # login by clicking on "Login" button
+        driver.find_element_by_class_name("nav-login").click()
+        
+        # wait for the frame to be available and switch to it
+        wait = WebDriverWait(driver, 15)
+        wait.until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "cboxIframe")))
+        # wait for presence of text field Username
+        driver.implicitly_wait(1)
+        wait.until(EC.presence_of_element_located((By.NAME, "username")))
+
+        # fill up the form on the frame
+        # we can find element by id, xpath, name, link text, class, css etc...
+        login_username = driver.find_element_by_name("username")
+        login_username.clear()
+        login_username.send_keys("TestUserName561")
+        login_password = driver.find_element_by_xpath("//input[contains(@placeholder, 'Enter your Password')]")
+        login_password.clear()
+        login_password.send_keys("TestPassword1")
+        driver.find_element_by_xpath("//button[contains(.,'Login')]").click()
+        
+        # return to main window
+        driver.switch_to_default_content()
+        # wait for page load and assert for text "Hello TestFirstName"
+        wait.until(EC.text_to_be_present_in_element((By.XPATH, "//p"), "TestFirstName"))
+        self.assertEqual("Hello TestFirstName", driver.find_element_by_xpath("//p").text)
+
+        # logout by clicking on "Logout" button
+        driver.find_element_by_class_name("nav-logout").click()
+
+        # assert for text "You've signed out successfully" on the main page after logging out
+        wait.until(EC.text_to_be_present_in_element((By.XPATH, "//article/div"), "signed out successfully"))
+        self.assertIn("You've signed out successfully", driver.find_element_by_xpath("//article/div").text)
+    
+    def tearDown(self):
+        pass
+        #self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
